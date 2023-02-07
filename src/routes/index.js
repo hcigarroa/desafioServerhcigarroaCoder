e219a2e9e4d7d32edbbeb228d226cc86dec5d5ab
hcigarroa <hcigarroa@gmail.com>
2023-02-07 16:58:19 -0600

    ServidorConBalance

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,3 +24,31 @@ process.memoryUsage();
 module.exports = router;
 
 
+let visitas = 0
+
+const calculo = function(){
+    let sum = 0
+    for(let i = 0; i < 5e9; i++){
+        sum += i
+    }
+    return sum
+}
+
+
+app.get("/", function(req, res){
+    visitas++
+    res.send("Visitas: " + visitas)
+})
+
+app.get("/calculo", function(req, res){
+    let sum = calculo()
+    res.send("LA sumaes es : ", sum)
+    // const comp =  fork("./calculo.js")
+    // comp.send("start")
+    // comp.on("message", function(sum){
+    //     res.send("la suma es : " + sum)
+    // })
+})
+app.listen(8080, function(){
+    console.log("Server run on port 8080")
+})
